05be35534eb7f905e3248146a359a4da9ef3ecd5
mrmclaine <lypovskyi@gmail.com>
2025-03-04 00:14:48 +0100

    feat/ added postgresSqlConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { graphqlConfig } from './config/graphql.config';
+import { postgresSqlConfig } from './config/postgresSqlConfig';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-    imports: [GraphQLModule.forRoot(graphqlConfig), UserModule],
+    imports: [
+        GraphQLModule.forRoot(graphqlConfig),
+        TypeOrmModule.forRoot(postgresSqlConfig),
+        UserModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
---
+++ src/config/postgresSqlConfig.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config();
+
+export const postgresSqlConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'nestjs_db',
+    entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+    synchronize: true,
+    logging: true,
+};

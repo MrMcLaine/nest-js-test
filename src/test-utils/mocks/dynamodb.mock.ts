92c02606aa028075dc777b70a76fbd4c0772ee01
mrmclaine <lypovskyi@gmail.com>
2025-03-06 15:13:25 +0100

    feat/ added UserActivityLogsService unit tests

--- src/test-utils/mocks/dynamodb.mock.ts
+++ src/test-utils/mocks/dynamodb.mock.ts
@@ -3,6 +3,7 @@ import { DynamoDBService } from '@dynamodb/dynamodb.service';
 export const dynamodbMock = {
     provide: DynamoDBService,
     useValue: {
+        queryTable: jest.fn(),
         scanTable: jest.fn(),
         putItem: jest.fn(),
         updateItem: jest.fn(),

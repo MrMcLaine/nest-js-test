99015a2629e28717768783c264057214cdd1a9cb
mrmclaine <lypovskyi@gmail.com>
2025-03-06 21:47:06 +0100

    refactor/ enum UserActionPermissions

--- src/modules/auth/constants/user-action-permissions.enum.ts
+++ src/modules/auth/constants/user-action-permissions.enum.ts
@@ -1,6 +1,4 @@
 export enum UserActionPermissions {
-    GET_ONE_BOOK = 'GET_ONE_BOOK',
-    GET_BOOKS = 'GET_BOOKS',
     CREATE_BOOK = 'CREATE_BOOK',
     UPDATE_BOOK = 'UPDATE_BOOK',
     DELETE_BOOK = 'DELETE_BOOK',

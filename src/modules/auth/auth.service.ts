638363c119ef5b9ba6f9452ddefbf33ccd44f80d
mrmclaine <lypovskyi@gmail.com>
2025-03-06 21:41:38 +0100

    fix/ login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { AuthResponse } from '@user/dto/auth-response.dto';
 import { toUserDto } from '@user/utils/toUserDto';
 import { toJwtPayload } from '@auth/utils/toJwtPayload';
 import { LoginAuthInput } from '@auth/dto/login-auth.input';
+import { BcryptUtil } from '@common/utils/bcrypt.util';
 
 @Injectable()
 export class AuthService {
@@ -21,6 +22,14 @@ export class AuthService {
 
     async login(input: LoginAuthInput): Promise<AuthResponse> {
         const user = await this.userService.findByEmailWithCheck(input.email);
+
+        const isPasswordValid = await BcryptUtil.comparePasswords(
+            input.password,
+            user.password
+        );
+
+        if (!isPasswordValid)
+            throw new Error(`The password ${input.password} is incorrect`);
 
         return {
             user: toUserDto(user),

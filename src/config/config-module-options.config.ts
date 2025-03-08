a6766adfb76ecc44f61e0946a3c84898a82a522a
mrmclaine <lypovskyi@gmail.com>
2025-03-08 13:47:54 +0100

    refactor/ configModuleOptions - using EnvName-enum

--- src/config/config-module-options.config.ts
+++ src/config/config-module-options.config.ts
@@ -1,28 +1,28 @@
-import { ConfigModuleOptions } from '@nestjs/config/dist/interfaces';
-import * as Joi from 'joi';
+import { EnvName } from '@common/enums/env-name.enum';
+import Joi from 'joi';
+import { ConfigModuleOptions } from '@nestjs/config';
 
 export const configModuleOptions: ConfigModuleOptions = {
     isGlobal: true,
     envFilePath: '.env',
     validationSchema: Joi.object({
-        PORT: Joi.number().default(5000),
+        [EnvName.PORT]: Joi.number().default(5000),
 
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.number().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().allow(''),
-        DB_NAME: Joi.string().required(),
+        [EnvName.DB_HOST]: Joi.string().required(),
+        [EnvName.DB_PORT]: Joi.number().required(),
+        [EnvName.DB_USERNAME]: Joi.string().required(),
+        [EnvName.DB_PASSWORD]: Joi.string().allow(''),
+        [EnvName.DB_NAME]: Joi.string().required(),
 
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRES_IN: Joi.string(),
+        [EnvName.JWT_SECRET]: Joi.string().required(),
+        [EnvName.JWT_EXPIRES_IN]: Joi.string(),
 
-        AWS_ACCESS_KEY_ID: Joi.string().required(),
-        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
-        AWS_REGION: Joi.string().allow(''),
-        AWS_DYNAMODB_TABLE: Joi.string().allow(''),
+        [EnvName.AWS_ACCESS_KEY_ID]: Joi.string().required(),
+        [EnvName.AWS_SECRET_ACCESS_KEY]: Joi.string().required(),
+        [EnvName.AWS_REGION]: Joi.string().allow(''),
 
-        REDIS_HOST: Joi.string().required(),
-        REDIS_PORT: Joi.number().required(),
-        REDIS_PASSWORD: Joi.string().allow(''),
+        [EnvName.REDIS_HOST]: Joi.string().required(),
+        [EnvName.REDIS_PORT]: Joi.number().required(),
+        [EnvName.REDIS_PASSWORD]: Joi.string().allow(''),
     }),
 };
